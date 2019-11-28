e2ddc8f22d658e883da2bf4d66620d578d5f54b8
pinkWarrior <38021042+seleneal1996@users.noreply.github.com>
2019-11-28 15:56:23 -0500

    IS2

---
+++ Kronos/app/src/main/java/com/geo/kronos/MainActivity.java
@@ -0,0 +1,99 @@
+package com.geo.kronos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //defining view objects
+    private EditText TextEmail;
+    private EditText TextPassword;
+    private Button btnRegistrar;
+    private ProgressDialog progressDialog;
+
+
+    //Declaramos un objeto firebaseAuth
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //inicializamos el objeto firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //Referenciamos los views
+        TextEmail = (EditText) findViewById(R.id.TxtEmail);
+        TextPassword = (EditText) findViewById(R.id.TxtPassword);
+
+        btnRegistrar = (Button) findViewById(R.id.botonRegistrar);
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching listener to button
+        btnRegistrar.setOnClickListener(this);
+    }
+
+    private void registrarUsuario(){
+
+
+        String email = TextEmail.getText().toString().trim();
+        String password  = TextPassword.getText().toString().trim();
+
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Se debe ingresar un email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Falta ingresar la contraseña",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        progressDialog.setMessage("Iniciando Sesion ...");
+        progressDialog.show();
+
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+
+                            Toast.makeText(MainActivity.this,"Inicio de Sesion Exitoso: "+ TextEmail.getText(),Toast.LENGTH_LONG).show();
+                        }else{
+
+                            Toast.makeText(MainActivity.this,"No se encontro el correo especificado ",Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        registrarUsuario();
+    }
+}
